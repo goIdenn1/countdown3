3965d54861e8fcd968dd2de71aaef709d8596df9
Jav <jt6xnk@virginia.edu>
2022-05-19 08:21:27 -0400

    api debugging

--- countdown3/weather-app/src/App.js
+++ countdown3/weather-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Search from './Search';
+import React from 'react';
 
 function App() {
   return (
--- countdown3/weather-app/src/Search.js
+++ countdown3/weather-app/src/Search.js
@@ -1,16 +1,38 @@
-import {useRef} from "react"
+import React, {useEffect, useRef, useState} from "react"
 import TextField from '@mui/material/TextField';
 
 const Search = (props) => {
-    /*london test code */
+    //states
+    const [apiData, setApiData] = useState({});
+    const [getLocation, setGetLocation] = useState();
+    const [location, setLocation] = useState();
     const key = process.env.REACT_APP_weather_key
-    const location = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={key}";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
+        /*london test code */
+    const location_url_test = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={key}";
     const textFieldRef = useRef();
-   
+   useEffect(()=>{
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => setApiData(data))
+   },[url])
+   const input = (event) =>
+   {
+       setGetLocation(event.target.value);
+   };
+   const submitInput = () => {
+       setLocation(getLocation);
+   };
+   const kelvinToF = (kelvin) =>{
+    return (kelvin - 273.15).toFixed(2);
+   };
     return (
+        //const textFieldRef = useRef();
+
         <div>
              <TextField id="myTextField" label="Text Field" variant="outlined" inputRef={textFieldRef} />
              <button onClick={() => console.log(textFieldRef.current.value)}>Log Input</button>
+             
         </div>
         );
 }
